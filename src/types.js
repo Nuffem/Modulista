3924b4b7c58b68fcb22e3263bbc64aef10d97a40
Ângelo Nuffer <angelonuffer@gmail.com>
2025-08-16 11:15:31 -0300

    Refatora(types): Corrige nomes de arquivos e restaura implementações de tipos 'Número' e 'Lógico'

--- src/types.js
+++ src/types.js
@@ -1,7 +1,7 @@
 import Lista from './types/Lista.js';
 import Texto from './types/Texto.js';
-import Número from './types/Número.js';
-import Lógico from './types/Lógico.js';
+import Número from './types/Numero.js';
+import Lógico from './types/Logico.js';
 
 const types = {
     Lista,
